044694d6ced81ecc957d688154ec0333fd0ef3b1
Thiago Lins <falecom@thiagolins.dev.br>
2022-08-16 02:10:27 -0300

    [FIX] Home Logo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import reactLogo from 'assets/react.svg'
 import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 
+import reactLogo from '../assets/react.svg'
 import GlobalStyles from './styles/global'
 import defaultTheme from './styles/themes/default'
+
 import './App.css'
 
 function App() {
