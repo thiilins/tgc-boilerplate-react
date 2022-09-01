c4376b85505b54044b6fa4cbf8a4196ab5071a41
Thiago Lins <falecom@thiagolins.dev.br>
2022-09-01 00:54:16 -0300

    [Fix] i18n Configuration

--- src/app/configs/i18n.ts
+++ src/app/configs/i18n.ts
@@ -1,11 +1,17 @@
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
+import { initReactI18next, useTranslation } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
+export const languages = {
+  name: 'Português',
+  alias: 'pt-BR',
+  flag: 'br'
+}
+
 i18n
   .use(Backend)
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'pt-BR',
@@ -13,5 +19,10 @@ i18n
       escapeValue: false // not needed for react as it escapes by default
     }
   })
+const t = (value: string) => {
+  const { t } = useTranslation()
+  return t(value)
+}
 
+export { t, useTranslation }
 export default i18n
