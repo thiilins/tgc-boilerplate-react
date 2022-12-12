4ea55982daa40d7e8dcf9a59175855369d48c027
Thiago Lins <falecom@thiagolins.dev.br>
2022-12-12 17:47:39 -0300

    [Update] Configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react({
+      include: '**/*.tsx'
+    }),
+    tsconfigPaths(),
+    svgrPlugin()
+  ]
 })
