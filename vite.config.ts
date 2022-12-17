d23dc6863310a2edbebd520ae66aa7fd59d0e33a
Thiago Lins <falecom@thiagolins.dev.br>
2022-12-17 16:01:53 -0300

    [Update] vite.config.ts [Fix] Dependencies Error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import svgr from 'vite-plugin-svgr'
 export default defineConfig({
   plugins: [
     react({
       include: '**/*.tsx'
     }),
     tsconfigPaths(),
-    svgrPlugin()
+    svgr()
   ]
 })
